d143368a82a746712f296309a358c02e84067892
Rich Insley <richard.insley@cs.inc>
2025-07-27 17:54:06 -0700

    added darwin arcana libs

---
+++ arcana/arcana_darwin.go
@@ -0,0 +1,38 @@
+//go:build darwin && cgo
+// +build darwin,cgo
+
+package arcana
+
+/*
+#cgo CFLAGS: -I${SRCDIR}/../../release/include -I${SRCDIR}/../../release/include/arcana
+#cgo LDFLAGS: ${SRCDIR}/../../release/lib/libavformat_arcana.dylib
+#cgo LDFLAGS: ${SRCDIR}/../../release/lib/libavcodec_arcana.dylib
+#cgo LDFLAGS: ${SRCDIR}/../../release/lib/libavfilter_arcana.dylib
+#cgo LDFLAGS: ${SRCDIR}/../../release/lib/libavdevice_arcana.dylib
+#cgo LDFLAGS: ${SRCDIR}/../../release/lib/libswresample_arcana.dylib
+#cgo LDFLAGS: ${SRCDIR}/../../release/lib/libswscale_arcana.dylib
+#cgo LDFLAGS: ${SRCDIR}/../../release/lib/libpostproc_arcana.dylib
+#cgo LDFLAGS: ${SRCDIR}/../../release/lib/libavutil_arcana.dylib
+
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavutil/avutil.h>
+#include <libavfilter/avfilter.h>
+#include <libavdevice/avdevice.h>
+#include <libpostproc/postprocess.h>
+#include <libswresample/swresample.h>
+#include <libswscale/swscale.h>
+*/
+import "C"
+import "fmt"
+
+func Platform_init() {
+	fmt.Printf("libavcodec version:    %d\n", uint(C.avcodec_version()))
+	fmt.Printf("libavformat version:   %d\n", uint(C.avformat_version()))
+	fmt.Printf("libavutil version:     %d\n", uint(C.avutil_version()))
+	fmt.Printf("libavfilter version:   %d\n", uint(C.avfilter_version()))
+	fmt.Printf("libavdevice version:   %d\n", uint(C.avdevice_version()))
+	fmt.Printf("libpostproc version:   %d\n", uint(C.postproc_version()))
+	fmt.Printf("libswresample version: %d\n", uint(C.swresample_version()))
+	fmt.Printf("libswscale version:    %d\n", uint(C.swscale_version()))
+}
